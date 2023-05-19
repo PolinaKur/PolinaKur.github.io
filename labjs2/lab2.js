e21affdb9dad29fa868b0d7e5cd9346a59c1d542
PolinaKur <126785905+PolinaKur@users.noreply.github.com>
2023-05-19 09:38:00 +0300

    yyy

--- labjs2/lab2.js
+++ labjs2/lab2.js
@@ -11,7 +11,7 @@ function pow(x, n) {
 let x = prompt("x?", '');
 let n = prompt("n?", '');
 
-if (n < 1) {
+if (n < 0) {
   alert(`Степень ${n} не поддерживается, используйте натуральное число`);
 } else {
   alert( pow(x, n) );
