91900205dccd6f27b7bf659275b9da3d9b83b4e8
PolinaKur <126785905+PolinaKur@users.noreply.github.com>
2023-05-25 09:42:57 +0300

    ooo

--- labjs2/lab2.js
+++ labjs2/lab2.js
@@ -4,3 +4,9 @@ function pow(x, n) {
 function sumTo(n) {
   return n * (n+1)/2;
 }
+function factorial(n) {
+  if  (n==0n || n==1n){
+  alert(1n);
+} else {
+  result= BigInt(n)*BigInt(factorial(n-1));
+}}
