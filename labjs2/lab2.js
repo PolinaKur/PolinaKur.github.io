ac17484f5419bcd7072341039014b8dd5affba95
PolinaKur <126785905+PolinaKur@users.noreply.github.com>
2023-05-25 09:10:34 +0300

    ooooo

--- labjs2/lab2.js
+++ labjs2/lab2.js
@@ -1,12 +1,6 @@
 function pow(x, n) {
   return x**n;
 }
-//let result = x;
-//let result = n;
-//if (n%1!==0) {
-  //alert(`Степень ${n} не поддерживается, используйте целое число`);
-//} else {
- // alert( pow(x, n) );
-//}
-//let result = pow(x, n);
-//alert( result );    
+function sumTo(n) {
+  return n * (n+1)/2;
+}
