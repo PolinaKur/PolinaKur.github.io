8c80e1648e1b620c9b70a182ea19cb7da9aefa7a
PolinaKur <126785905+PolinaKur@users.noreply.github.com>
2023-05-25 09:58:21 +0300

    jjj

--- labjs2/lab2.js
+++ labjs2/lab2.js
@@ -5,8 +5,8 @@ function sumTo(n) {
   return n * (n+1)/2;
 }
 function factorial(n) {
-  if  (n==0n || n==1n){
-    let result = 1n; 
-} else {
-  result= BigInt(n)*BigInt(factorial(n-1));
+  if  (n<1) return 1n;
+  else {
+  result= BigInt(n)*BigInt(factorial(n-1))
+  return result;
 }}
