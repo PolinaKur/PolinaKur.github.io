8cc356eba088c16d455cba7927e2fa0872cdfcaf
PolinaKur <126785905+PolinaKur@users.noreply.github.com>
2023-05-25 10:51:26 +0300

    ооо

--- labjs2/lab2.js
+++ labjs2/lab2.js
@@ -1,18 +1,16 @@
 function pow(x, n) {
-  return x**n;
+  return x ** n; //возводим число x в степень n
 }
 function sumTo(n) {
-  return n * (n+1)/2;
+  return n * (n + 1) / 2;
 }
 function factorial(n) {
-  if  (n<1) return 1n;
+  if (n < 1) return 1n;
   else {
-  result= BigInt(n)*BigInt(factorial(n-1))
-  return result;
-}}
+    result = BigInt(n) * BigInt(factorial(n - 1))
+    return result;
+  }
+}
 function fib(n) {
-  if  (n<=1) return 1n;
-  else {
-  result= BigInt(fib(n-1))+BigInt(fib(n-2))
-  return result;
-}}
+  return n <= 1 ? n : fib(n - 1) + fib(n - 2);
+}
