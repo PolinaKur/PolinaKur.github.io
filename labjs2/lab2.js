9b2247488bf4bf4156436ca9ed0c4fd6a9904a87
PolinaKur <126785905+PolinaKur@users.noreply.github.com>
2023-05-26 12:37:59 +0300

    kkkk

--- labjs2/lab2.js
+++ labjs2/lab2.js
@@ -33,5 +33,13 @@ function compare(x) {
     else return alert('Something wrong');
   }
 }
+function sum() {
+  var result = 0;
 
+  for (var i = 0; i < arguments.length; i++) {
+    result += arguments[i];
+  }
+
+  return result;
+}
 
