89fc142f46190b0e44177704044fc0668522261b
PolinaKur <126785905+PolinaKur@users.noreply.github.com>
2023-05-26 13:07:11 +0300

    hhh

--- labjs2/lab2.js
+++ labjs2/lab2.js
@@ -2,7 +2,7 @@ function pow(x, n) {
   return x ** n; //возводим число x в степень n
 }
 function sumTo(n) {
-  return n * (n + 1) / 2;
+  return n * (n + 1) / 2; //
 }
 function factorial(n) {
   if (n < 1) return 1n;
