913cda876fa3f1af00e4a3839750f4653a0a8ae8
PolinaKur <126785905+PolinaKur@users.noreply.github.com>
2023-06-02 15:00:30 +0300

    фвв

--- labjs3/lab3.js
+++ labjs3/lab3.js
@@ -1,26 +1,12 @@
-//import {
-  //  fib
-//} from './lab2.js';
+import {fib} 
+from './lab2.js';
 
-/**
- * Возвращает дробную часть числа num.
- *
- * @param {number} num число, от которого нужно найти дробную часть.
- * @return {number} дробная часть числа.
- */
 export function getDecimal(num) {
     if (Math.trunc(num) == num) return 0;
     else if (num < 0) return num - (-Math.ceil(-num));
     if (num > 1) return +(num - Math.trunc(num)).toFixed(2);
 }
 
-
-/**
- * Возвращает строку str с заглавным первым символом.
- *
- * @param {string} str строка, которую нужно преобразовать.
- * @return {string} str с заглавным первым символом.
- */
 export function ucFirst(str) {
     if (!str) return str;
     else {
