b3ccec690a20b2cd012e6a94afafc8fca952c76d
PolinaKur <126785905+PolinaKur@users.noreply.github.com>
2023-06-02 13:12:52 +0300

    vvv

--- labjs3/lab3.js
+++ labjs3/lab3.js
@@ -3,6 +3,8 @@ export function getDecimal(num) {
     else if (num < 0) return num - (-Math.ceil(-num));
     if (num > 1) return +(num - Math.trunc(num)).toFixed(2);
 }
+x
+
 export function ucFirst(str) {
     if (!str) return str;
     return str[0].toUpperCase() + str.slice(1);
