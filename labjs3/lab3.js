8c108e72091c7a21fffd33c282ddabf16e60b767
PolinaKur <126785905+PolinaKur@users.noreply.github.com>
2023-06-02 12:09:34 +0300

    add

--- labjs3/lab3.js
+++ labjs3/lab3.js
@@ -1,3 +1,9 @@
-export function getDecimal {
-    
-}
+export function getDecimal(num) {
+    var str = "" + num; // 1
+    var zeroPos = str.indexOf("."); // 2
+   
+    if (zeroPos == -1) return 0; // 3
+    str = str.slice(zeroPos); // 4
+   
+    return +str; // 5
+   }
